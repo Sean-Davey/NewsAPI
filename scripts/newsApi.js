eaee77ec66c9f978e6ed923ded7f41778e9cfccf
Sean Davey <seananthonydavey@gmail.com>
2019-03-06 13:09:57 +0000

    Search and other categories now working using same AJAX call

--- scripts/newsApi.js
+++ scripts/newsApi.js
@@ -3,31 +3,91 @@ var apiQuery='top-headlines';
 var pageSize='&pageSize=3';
 const apiKey='&apiKey=30d24c47ec594157b6f4ce2f2cbb1588';
 
+
+var dataSize=3;
+
+var topic = 'World Headlines'
+
 // backupKey 1c6b7c4371634281b57f26aa8cd9210e
 
 $(document).ready(function() {
-TopNewsRequest();
+    newsRequest();
 });
+
 
 $('.navbar-brand').click(function() {
     location.reload();
 })
 
-$("#Entertainment").click(function() {
-
-apiQuery='entertainment';
-
-console.log(apiQuery);
+$('#Search').click(function() {
+    var searchValue = $(".c-news-search").val()
+    apiQuery = searchValue;
 
     $(".card").fadeOut(800).promise()
     .then(function() {
-        TopNewsRequest();
+        newsRequest();
     });
 })
 
+$("#Home").click(function() {
+
+    if(apiQuery === apiQuery) 
+
+    apiQuery='top-headlines';
+    topic='World Headlines'
+    
+    console.log(apiQuery);
+    
+        $(".card").fadeOut(800).promise()
+        .then(function() {
+            newsRequest();
+        });
+    })
+
+$("#Tech").click(function() {
+
+    apiQuery='technology';
+    topic='Technology Headlines'
+    //pageSize='&pageSize=2';
+    //dataSize=2;
+    
+    console.log(apiQuery);
+    
+        $(".card").fadeOut(800).promise()
+        .then(function() {
+            newsRequest();
+        });
+    })
+
+    $("#Politics").click(function() {
+
+        apiQuery='politics';
+        topic='Politics Headlines'
+        
+        console.log(apiQuery);
+        
+            $(".card").fadeOut(800).promise()
+            .then(function() {
+                newsRequest();
+            });
+        })
+
+        $("#Entertainment").click(function() {
+
+            apiQuery='entertainment';
+            topic='Entertainment Headlines'
+            
+            console.log(apiQuery);
+            
+                $(".card").fadeOut(800).promise()
+                .then(function() {
+                    newsRequest();
+                });
+            })
+
 // TOP NEWS WORLDWIDE 
 
-    function TopNewsRequest() {
+    function newsRequest() {
         
         var url = apiPath + apiQuery + pageSize + apiKey;
        
@@ -45,9 +105,9 @@ console.log(apiQuery);
                 
                  console.dir(data);
 
-                 $('.c-news-card-title').html('World Headlines')
+                 $('.c-news-card-title').html(topic)
                  
-                 for(let n = 0; n < 3; n++)
+                 for(let n = 0; n < dataSize; n++)
                  {
                   let storyTitle = data.articles[n].title
                   let storyImage = data.articles[n].urlToImage
@@ -62,14 +122,14 @@ console.log(apiQuery);
                     "<h5 class='card-title'>'" + storyTitle + "'</h5>" +
                     "<hr/>" +
                     "<p class='card-text'>'" + storyDescription + "'</p>" +
-                    "<p class='card-text-sm' id="+ n +"> See More </p>" +
+                    "<a href='#ex1' rel='modal:open' class='card-text-sm' id="+ n +"> See More </a>" +
                     "</div>" +
                     "<div class='card-footer'>" +
                     "<small class='text-muted'>Published : '" + storyDate + "'</small>" +
                     "<div class='c-news-card-modal-hide'> '" + storyContent + "</div>" +
                     "</div>" + 
                     "</div>"
-                    ).fadeIn(1000)  
+                    ).fadeIn(2000)  
                 } 
               
                 $('.card-text-sm').click(function() {  
@@ -78,7 +138,7 @@ console.log(apiQuery);
 
                     console.log(newsItemid);
 
-                    for(let n = 0; n < 3; n++) {
+                    for(let n = 0; n < dataSize; n++) {
                         let storyContent = data.articles[n].content
                         if(newsItemid == newsID[n]) {
                             alert("Clicked :" + storyContent)
@@ -89,132 +149,6 @@ console.log(apiQuery);
             }
             })
            }
-
-// TECHNOLOGY NEWS 
-
-function TechnologyRequest() {
-
-    var apiPath='https://newsapi.org/v2/everything?q=';
-    var apiQuery='technology';
-    var pageSize='&pageSize=3';
-    const apiKey='&apiKey=30d24c47ec594157b6f4ce2f2cbb1588';
-
-    var Techurl = apiPath + apiQuery + pageSize + apiKey;
-
-    $.ajax(
-        {
-         type: "GET",
-         dataType: "json",
-         cache: false, 
-         url: Techurl,
-         success: function(data)
-         {
-     
-             console.dir(data);
-
-             $('.c-news-card-title').html('Technology Headlines')
-             
-             for(var t = 0; t < 3; t++)
-             {
-             var storyTitle = data.articles[t].title
-             var storyImage = data.articles[t].urlToImage
-             var storyDescription = data.articles[t].description
-             var storyDate = data.articles[t].publishedAt
-
-             $('.card-deck').append(
-                "<div class='card'>" +
-                "<img class='card-img-top' src='" + storyImage + "' alt='Card image cap'> " +
-                "<div class='card-body'>" +
-                "<h5 class='card-title'>'" + storyTitle + "'</h5>" +
-                "<hr/>" +
-                "<p class='card-text'>'" + storyDescription + "'</p>" +
-                "<p class='card-text-sm'> See More </p>" +
-                "</div>" +
-                "<div class='card-footer'>" +
-                "<small class='text-muted'>Published : '" + storyDate + "'</small>" +
-                "</div>" + 
-                "</div>"
-                ).fadeIn(4000)
-            }       
-        }
-        }
-         )
-    }
-
-// POLITICS NEWS 
-
-    function PoliticsRequest() {
-
-        var apiPath='https://newsapi.org/v2/everything?q=';
-        var apiQuery='politics';
-        var pageSize='&pageSize=3';
-        const apiKey='&apiKey=30d24c47ec594157b6f4ce2f2cbb1588';
-    
-        var Techurl = apiPath + apiQuery + pageSize + apiKey;
-    
-        $.ajax(
-            {
-             type: "GET",
-             dataType: "json",
-             cache: false, 
-             url: Techurl,
-             success: function(data)
-             {
-         
-                 console.dir(data);
-                
-                 $('.c-news-card-title').html('Political Headlines')
-                
-                 for(var t = 0; t < 3; t++)
-                 {
-                 var storyTitle = data.articles[t].title
-                 var storyImage = data.articles[t].urlToImage
-                 var storyDescription = data.articles[t].description
-                 var storyDate = data.articles[t].publishedAt
-    
-                 $('.card-deck').append(
-                    "<div class='card'>" +
-                    "<img class='card-img-top' src='" + storyImage + "' alt='Card image cap'> " +
-                    "<div class='card-body'>" +
-                    "<h5 class='card-title'>'" + storyTitle + "'</h5>" +
-                    "<hr/>" +
-                    "<p class='card-text'>'" + storyDescription + "'</p>" +
-                    "<p class='card-text-sm'> See More </p>" +
-                    "</div>" +
-                    "<div class='card-footer'>" +
-                    "<small class='text-muted'>Published : '" + storyDate + "'</small>" +
-                    "</div>" + 
-                    "</div>"
-                    ).fadeIn(4000)
-                }       
-            }
-            }
-             )
-        }
-
-// CONTENT NEWS AREA SELECTION
-
-$("#Home").click(function() {
-    $(".card").fadeOut(800).promise()
-    .then(function() {
-        TopNewsRequest();
-    });
-})
-
-$("#Tech").click(function() {
-    $(".card").fadeOut(800).promise()
-    .then(function() {
-        TechnologyRequest();        
-    });
-})
-
-$("#Politics").click(function() {
-    $(".card").fadeOut(800).promise()
-    .then(function() {
-        PoliticsRequest();
-    });
-})
-
 
 // SEARCH FUNCTION 
 
